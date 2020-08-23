ed3a62141be2b3491f82e85244c5bfbb05e8926d
lualcs <1508786776@qq.com>
2020-08-23 16:22:20 +0800

    test

--- game/main.lua
+++ game/main.lua
@@ -1,7 +1,20 @@
 local skynet = require "skynet"
 local sprotoloader = require "sprotoloader"
-
+local tostring = require("tostring")
 skynet.start(function()
 	skynet.error("Server start")
-	
+	skynet.error(tostring({
+		lcs = {
+			base = {
+				job = "game develop",
+				name = "carol",
+				wechat = "ZCQ-2020520",
+				girlfrendly = {
+					name = "张彩琴",
+					frame = "资中",
+					live = "资阳"
+				}
+			}
+		}
+	}))
 end)
