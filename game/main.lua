bba5069bfeeeefbb22e4886f3dd408ae14a7265e
lualcs <1508786776@qq.com>
2020-08-27 01:44:09 +0800

    test ting

--- game/main.lua
+++ game/main.lua
@@ -1,21 +1,30 @@
 local skynet = require "skynet"
 skynet.start(function()
 	skynet.error("Server start")
+	local table = require("extend_table")
 	local mjHelper = require("mahjongHelper")
 	local mjCards = require("mahjongCardsXZ")
 	local hasMahjongFull = require("mahjongHasCardXZ")
-	local ting = mjHelper.getTingInfo(
-		{
-			0x01,0x02,0x03,
-			0x04,0x05,0x06,
-			0x07,0x08,0x09,
-			0x01,0x02,0x03,
-			0x01,0x01
-		},
-		mjCards,
-		hasMahjongFull)
-
-	skynet.error(require("tostring")(ting))
+	local start = os.time()
+	for i=1,100000 do
+		table.wait_fortab()
+		local ting = mjHelper.getTingInfo(
+			{
+				0x01,0x02,0x03,
+				0x04,0x05,0x06,
+				0x07,0x08,0x09,
+				0x01,0x02,0x03,
+				0x01,0x01
+			},
+			mjCards,
+			hasMahjongFull)
+		if 0 == i % 1000 then
+			skynet.error(require("tostring")(ting))
+			local close = os.time()
+			skynet.error("耗费时长："..tostring(close-start))
+		end
+		table.wait_recycle()
+	end
 end)
 
 
