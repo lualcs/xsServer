728e69b586c23b0b3d0d78342f9a2b05b00a05e4
lcs-new <1508786776@qq.com>
2021-02-06 14:01:53 +0800

    ranking

---
+++ lualib/common/custom/ranking.lua
@@ -0,0 +1,14 @@
+--[[
+    file:ranking.lua 
+    desc:排行榜
+    auth:Carol Luo
+]]
+
+local class = require("class")
+local ranking = class()
+
+---构造函数
+function ranking:ctor()
+end
+
+return ranking
