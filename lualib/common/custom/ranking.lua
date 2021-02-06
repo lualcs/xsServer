b110663273f13f9a919d803397eaa6532f6b3367
lcs-new <1508786776@qq.com>
2021-02-06 14:11:47 +0800

    code

--- lualib/common/custom/ranking.lua
+++ lualib/common/custom/ranking.lua
@@ -4,11 +4,23 @@
     auth:Carol Luo
 ]]
 
+local tsort = require("sort")
 local class = require("class")
 local ranking = class()
 
 ---构造函数
-function ranking:ctor()
+---@param max   count @最大数量
+function ranking:ctor(max)
+    ---@type count  @最大数量
+    self._max = max;
+    ---@type rank[] @排行数据
+    self._lis = {};
+end
+
+---更新数据
+---@param iden  userID @用户ID
+---@param score score  @积分
+function ranking:update(iden,score)
 end
 
 return ranking
