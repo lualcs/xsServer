8df5e9a5524171d5aa0e1c2566cb383b1d472366
lualcs <1508786776@qq.com>
2021-02-20 04:35:58 -0800

    自增长整数

--- lualib/common/custom/fauto.lua
+++ lualib/common/custom/fauto.lua
@@ -4,8 +4,12 @@
     auth:Carol Luo
 ]]
 
+---自增长函数
+---@return function
 return function()
     local auto = 0
+    ---获取自增长正整数
+    ---@return number
     return function()
         auto=auto+1
         return auto
