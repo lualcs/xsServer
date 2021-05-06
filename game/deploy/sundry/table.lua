849e3d974c63a9f0d9160f2c18ac4a58a8e9696b
lualcs <1508786776@qq.com>
2021-05-06 07:01:49 -0700

    table

--- game/deploy/sundry/table.lua
+++ game/deploy/sundry/table.lua
@@ -5,38 +5,41 @@
 
 local senum = require("managerEnum")
 
----@type table<gameID,tableManagerInfo>
+---@type table<senum,tableManagerUnit[]>
 return {
-    ---炸金花
-    [1] = {
-        assignClass = senum.assignKilling(),
-        list = {
-            [1] = {
-                unit = 1,   ---单元积分
-                mini = 2,   ---最少桌子
-                maxi = 100, ---最多桌子
-                idle = 2,   ---保持空闲
-            },
-            [2] = {
-                unit = 5,   ---单元积分
-                mini = 2,   ---最少桌子
-                maxi = 100, ---最多桌子
-                idle = 2,   ---保持空闲
-            },
-        }
+    ---竞技场
+    [senum.assignKilling()] = {
+        [1] = {
+            unit = 1,   ---单元积分
+            mini = 2,   ---最少桌子
+            maxi = 100, ---最多桌子
+            idle = 2,   ---保持空闲
+        },
+        [2] = {
+            unit = 5,   ---单元积分
+            mini = 2,   ---最少桌子
+            maxi = 100, ---最多桌子
+            idle = 2,   ---保持空闲
+        },
         
     },
-    ---龙虎斗
-    [20001] = {
-        assignClass = senum.assignKilling(),
-        list = {
-            [1] = {
-                unit = 1,   ---单元积分
-                mini = 3,   ---最少桌子
-                maxi = 3,   ---最多桌子
-                idle = nil, ---保持空闲
-            },        
-        },
+    ---百人场
+    [senum.assignHundred()] = {
+        [1] = {
+            unit = 1,   ---单元积分
+            mini = 3,   ---最少桌子
+            maxi = 3,   ---最多桌子
+            idle = nil, ---保持空闲
+        },        
+    },
+     ---单人场
+     [senum.assignSingle()] = {
+        [1] = {
+            unit = 1,   ---单元积分
+            mini = 1,   ---最少桌子
+            maxi = 1,   ---最多桌子
+            idle = nil, ---保持空闲
+        },        
     }
 }
 
@@ -45,8 +48,3 @@ return {
 ---@field mini  count   @最少桌子
 ---@field maxi  count   @最多桌子
 ---@field idle  count   @保持空闲
-
-
----@class tableManagerInfo
----@field assignClass   senum               @分配类型
----@field list          tableManagerUnit[]  @桌子管理
