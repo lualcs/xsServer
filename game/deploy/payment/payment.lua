846460c389950897f400c7a9cb7122e41b33e9ed
lualcs <1508786776@qq.com>
2021-07-13 09:47:46 -0700

    支付配置

--- game/deploy/payment/payment.lua
+++ game/deploy/payment/payment.lua
@@ -3,8 +3,25 @@
     auth:Carol Luo
 ]]
 
+---支付配置
+---@type table<name,payPalt>
 return {
-
     ---玖通银联
-    {host="https://pay.namaye.cn/api/index"}
+    ["jtyl"] = {
+        reqURL="https://pay.namaye.cn/api/index",       --支付请求地址
+        bakURL="https://pay.namaye.cn/api/index",       --支付完成回调
+        fmtMD5 = "",                                    --支付加密匹配
+        fmtTXT = "",                                    --支付参数匹配
+        argAPP = "",                                    --支付商家用户                                   
+        argRMB = {},                                    --支付支持面额
+    },
 }
+
+
+---@class payPalt @支付平台
+---@field reqURL url        @支付请求地址
+---@field bakURL url        @支付完成回调
+---@field fmtMD5 string     @支付加密匹配
+---@field fmtTXT string     @支付参数匹配
+---@field argAPP string     @支付商家用户
+---@field argRMB number[]   @支付支持面额
--- game/deploy/payment/withdraw.lua
+++ game/deploy/payment/withdraw.lua
@@ -3,6 +3,27 @@
     auth:Carol Luo
 ]]
 
+---支付配置
+---@type table<name,withdrawPlat>
 return {
-    
+    ---玖通银联
+    ["jtyl"] = {
+        reqURL="https://pay.namaye.cn/api/index",       --提现请求地址
+        bakURL="https://pay.namaye.cn/api/index",       --提现完成回调
+        fmtMD5 = "",                                    --提现加密匹配
+        fmtTXT = "",                                    --提现参数匹配
+        argAPP = "",                                    --提现商家用户                                   
+        argRMB = {},                                    --提现支持面额
+        argCNT = {},                                    --提现货币数量
+    },
 }
+
+
+---@class withdrawPlat @提现平台
+---@field reqURL url        @支付请求地址
+---@field bakURL url        @支付完成回调
+---@field fmtMD5 string     @支付加密匹配
+---@field fmtTXT string     @支付参数匹配
+---@field argAPP string     @支付商家用户
+---@field argRMB number[]   @支付支持面额
+---@field argCNT number[]   @提现货币数量
